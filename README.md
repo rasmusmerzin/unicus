8ee51fe720dc0f0fc255b25c6e110ab713d2be77
Emil Rasmus Merzin <rasmusmerzin@gmail.com>
2025-04-19 21:39:06 +0300

    readme: added feature list

--- README.md
+++ README.md
@@ -8,6 +8,22 @@ including some features missing in existing authenticator apps, like proper
 encryption and backups. Unicus supports HOTP and TOTP, making it compatible with
 thousands of services.
 
+## Features
+
+- Encrypted vault
+  - With password and optional biometric authentication
+- Add OTP entries manually
+- Scan QR codes with camera or from image
+  - Single OTP
+  - Multiple OTP migration (Google Authenticator)
+- View entries' QR codes
+- Export entries to migration QR codes (Google Authenticator)
+- Export vault to a file
+- Import vault from a file
+- Automatically lock vault after inactivity (and optionally on app hide)
+- Dark mode
+- OTP issuer icons (from [aegis-icons](https://aegis-icons.github.io))
+
 ## Screenshots
 
 <div>
--- src/icons.ts
+++ src/icons.ts
@@ -61,12 +61,6 @@ export const chevronLeft = (size = 24) =>
 export const chevronRight = (size = 24) =>
   `<svg width="${size}px" height="${size}px" viewBox="0 -960 960 960"><path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/></svg>`;
 
-export const chevronUp = (size = 24) =>
-  `<svg width="${size}px" height="${size}px" viewBox="0 -960 960 960"><path d="M480-528 296-344l-56-56 240-240 240 240-56 56-184-184Z"/></svg>`;
-
-export const chevronDown = (size = 24) =>
-  `<svg width="${size}px" height="${size}px" viewBox="0 -960 960 960"><path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z"/></svg>`;
-
 export const scanQr = (size = 24) =>
   `<svg width="${size}px" height="${size}px" viewBox="0 -960 960 960"><path d="M80-680v-200h200v80H160v120H80Zm0 600v-200h80v120h120v80H80Zm600 0v-80h120v-120h80v200H680Zm120-600v-120H680v-80h200v200h-80ZM700-260h60v60h-60v-60Zm0-120h60v60h-60v-60Zm-60 60h60v60h-60v-60Zm-60 60h60v60h-60v-60Zm-60-60h60v60h-60v-60Zm120-120h60v60h-60v-60Zm-60 60h60v60h-60v-60Zm-60-60h60v60h-60v-60Zm240-320v240H520v-240h240ZM440-440v240H200v-240h240Zm0-320v240H200v-240h240Zm-60 500v-120H260v120h120Zm0-320v-120H260v120h120Zm320 0v-120H580v120h120Z"/></svg>`;
 
